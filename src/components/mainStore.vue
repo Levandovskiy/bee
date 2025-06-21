743edfdcfc51eef8a595201465c079a3cc966ef3
Levandovskiy <levandovskiy.bogdan.i@gmail.com>
2025-06-21 22:47:54 +0300

    add.main.Component

--- src/components/mainStore.vue
+++ src/components/mainStore.vue
@@ -1,5 +1,11 @@
 <template>
-  <img src="../assets/img/header/фон.png" alt="" />
+  <img class="img__header" src="../assets/img/header/фон.png" alt="" />
 </template>
 <script setup></script>
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.img__header {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
